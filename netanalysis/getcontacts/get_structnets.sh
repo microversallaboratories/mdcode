3f63a158816612942a121ee9879fb17cb9a8ea5c
Jacob North <microversallaboratories@gmail.com>
2020-12-14 21:10:37 -0800

    add structnet calculator, env.yml

--- netanalysis/getcontacts/get_structnets.sh
+++ netanalysis/getcontacts/get_structnets.sh
@@ -1,4 +1,6 @@
 # setup conda environment
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+
 conda activate getcontacts
 
 # set variables
@@ -10,27 +12,27 @@ cores=2
 
 # run getcontacts.py scripts
 
-################################################### <get_dyn> ##############################################
+################################################### <get_stat> ##############################################
 
-for fn in */*.pdb
+for fn in data/JLN_1_21_1B/*/*.pdb
 do
     # salt bridges
-    get_static_contacts.py --structure ${fn}.pdb --output statcont_sb_${fn}.tsv --solv HOH --itypes sb
+    get_static_contacts.py --structure ${fn} --output ${fn/.pdb/}_statcont_sb.tsv --itypes sb --sele2 'not (protein or nucleic or solv or lipid)'
 
     # pi-cation
-    get_static_contacts.py --structure ${fn}.pdb --output statcont_pc_${fn}.tsv --solv HOH --itypes pc
+    get_static_contacts.py --structure ${fn} --output ${fn/.pdb/}_statcont_pc.tsv --itypes pc --sele2 'not (protein or nucleic or solv or lipid)'
 
     # pi stacking
-    get_static_contacts.py --structure ${fn}.pdb --output statcont_ps_${fn}.tsv --solv HOH --itypes ps
+    get_static_contacts.py --structure ${fn} --output ${fn/.pdb/}_statcont_ps.tsv --itypes ps --sele2 'not (protein or nucleic or solv or lipid)'
 
     # t-stacking
-    get_static_contacts.py --structure ${fn}.pdb --output statcont_ts_${fn}.tsv --solv HOH --itypes ts
+    get_static_contacts.py --structure ${fn} --output ${fn/.pdb/}_statcont_ts.tsv --itypes ts --sele2 'not (protein or nucleic or solv or lipid)'
 
     # hydrophobic
-    get_static_contacts.py --structure ${fn}.pdb --output statcont_hp_${fn}.tsv --solv HOH --itypes hp
+    get_static_contacts.py --structure ${fn} --output ${fn/.pdb/}_statcont_hp.tsv --itypes hp --sele2 'not (protein or nucleic or solv or lipid)'
 
     # van der Waals
-    get_static_contacts.py --structure ${fn}.pdb --output statcont_hp_${fn}.tsv --solv HOH --itypes hp
+    get_static_contacts.py --structure ${fn} --output ${fn/.pdb/}_statcont_hp.tsv --itypes hp --sele2 'not (protein or nucleic or solv or lipid)'
 
     #   --solv HOH 
     #   --sele "chain A"
